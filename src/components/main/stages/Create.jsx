0e11be33dae975b322f8f4337762df3e953e6d2c
Alejandra Botacura <alejandrabotacura@outlook.com.ar>
2023-06-24 12:59:37 -0300

    Working server conection in create

--- src/components/main/stages/Create.jsx
+++ src/components/main/stages/Create.jsx
@@ -1,8 +1,33 @@
+import { useEffect,useState } from "react"
 import React from 'react'
-
+import axios from "axios"
 export const Create = () => {
+    
+    const [name,setName]=useState('')
+    const [description,setDescription]=useState('')
+    const fetchData= () => {axios.post('http://localhost:3001/post/newcharacter',{name: name, description: description}).then(
+    (response)=>{if 
+      (response.status==200) 
+      {window.alert("Agregado con exito")} 
+        else if (response.status==500)
+      {window.alert("Something went wrong with the server. we apology")}},
+    (error)=>{window.alert(error)}
+  )}
+    
   return (
-    <div>Create</div>
+
+    <article className="create-form-wrapper">
+      <h3>Crear personaje</h3>
+      <label>Nombre:</label>
+      <input type='text' placeholder='Ej. Superman' onChange={(e)=>{
+        setName(e.target.value)
+      }}/>
+      <label>Descripcion (opcional)</label>
+      <input type='text' placeholder='Ej. Hombre volador' onChange={(e)=>{
+        setDescription(e.target.value)
+      }}/>
+      <button onClick={()=>{fetchData()}}>Guardar en la base de datos</button>
+    </article>
   )
 }
 
