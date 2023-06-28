b662a7cae1f6dc02f9ef36c613fbb9b3a8471b4a
Alejandra Botacura <alejandrabotacura@outlook.com.ar>
2023-06-27 22:46:24 -0300

    Added endpoints and uodate and delete feature

--- src/components/main/stages/Delete.jsx
+++ src/components/main/stages/Delete.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export const Delete = () => {
-  return (
-    <div>Delete</div>
+  const [deleteID, setDeleteID] = useState()
+  const [IDListDelete, setIDListDelete] = useState([])
+  useEffect(()=>{
+    axios.get('http://localhost:3001/get/ids').then(
+      (response)=>{
+        setIDListDelete(response.data)
+      }, (error)=>{
+        console.log(error)
+      }
+    )},[])
+  const deleteIdDb=()=>{ axios.delete('http://localhost:3001/delete/characters/id/'+deleteID).then(
+    (response)=>{
+      if (response.status==200){
+        window.alert("deleted with success")
+        
+      } else if (response.status==500){
+        window.alert("error from the server")
+      } (error) =>{
+        console.log(error)
+      }
+    }
+  )
+
+  }
+return (
+    <article className='create-form-wrapper'>
+  <label>Id of the element you want to delete</label>
+  <select onChange={(e)=>{setDeleteID(e.target.value)}}>
+    <option value="">Select an existent id</option>
+      {IDListDelete && IDListDelete.map((e)=>{ return <option key={e.ID} value={e.ID}>{e.ID}</option>
+      })}
+  </select>
+  <button onClick={()=>{deleteIdDb()}}>DELETE</button>
+    </article>
   )
 }
 
--- src/components/main/stages/Update.jsx
+++ src/components/main/stages/Update.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 export const Update = () => {
-  return (
-    <div>Update</div>
+  const [IDList,setIDList]=useState([])
+  const [updateName, setUpdateName]=useState("")
+  const [updateDescription, setUpdateDescription]=useState("")
+  const [updateID, setUpdateID]=useState()
+
+  useEffect(()=>{
+    axios.get('http://localhost:3001/get/ids').then(
+      (response)=>{
+        setIDList(response.data)
+        console.log(response)
+      }, (error)=>{
+        console.log(error)
+      }
+    )},[])
+  const fetchUpdate= () =>{axios.put('http://localhost:3001/put/characters/id/'+updateID, {name: updateName, description: updateDescription}).then(
+      (response)=>{
+        if (response.status==200){
+          window.alert("Updated succesfully")
+          
+        } else if (response.status==500){
+          window.alert("Sorry, server error, try again!")
+        }
+      }, (error)=>{
+        console.log(error)
+        }
+    )
+  }
+  return (<article className='create-form-wrapper'>
+    <label>Id of the element you want to change</label>
+  <select onChange={(e)=>{setUpdateID(e.target.value)}}>
+    <option value="">Select an existent id</option>
+      {IDList && IDList.map((e)=>{ return <option key={e.ID} value={e.ID}>{e.ID}</option>
+      })}
+  </select> 
+    <label htmlFor="name">Nuevo Nombre (obligatorio)</label>
+    <input type="text" name="name" required onChange={(e)=>{
+      setUpdateName(e.target.value)
+    }}/>
+    <label htmlFor="description">Nueva Descripcion(opcional)</label>
+    <input type="tect" name="description" onChange={(e)=>{
+      setUpdateDescription(e.target.value)
+    }}/>
+    <button disabled={!updateName || !updateID} onClick={()=>{fetchUpdate()}}>Actualizar</button>
+    </article>
   )
 }
